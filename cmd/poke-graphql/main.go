63f777470b5e3270a238262eaf8338d445dc088a
kohdice <kohdice@gmail.com>
2024-05-05 00:54:19 +0900

    feat: Add database connection logic to poke-graphql/main.go

--- cmd/poke-graphql/main.go
+++ cmd/poke-graphql/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/kohdice/poke-graphql/internal/config"
 	"github.com/kohdice/poke-graphql/internal/graphql"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mysqldialect"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 func main() {
@@ -17,8 +21,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	databaseURL := cfg.DatabaseURL()
 	port := cfg.Port()
 	debug := cfg.Debug()
+
+	sqldb, err := sql.Open("mysql", databaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := bun.NewDB(sqldb, mysqldialect.New())
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(debug)))
 
 	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}}))
 
